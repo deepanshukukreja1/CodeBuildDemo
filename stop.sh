c4633e8b1959e8de6fdf5bc4f26ff7c3af7097db
Deepanshu <kukreja@apple.com>
2019-10-04 12:17:47 -0700

    changed

--- stop.sh
+++ stop.sh
@@ -1,8 +1,15 @@
 echo "Checking if the deployed version is same as of build folder"
 mkdir -p /home/ec2-user/builds/log/
 
-deployedBuild=`basename /home/ec2-user/builds/lib/messageUtil-1.0.jar|cut -d '-' -f2|sed 's/.jar//'`
-deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
+export deployedBuild=`basename /home/ec2-user/builds/lib/messageUtil-1.0.jar|cut -d '-' -f2|sed 's/.jar//'`
+export deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
 
 echo application has been stop  >> /home/ec2-user/builds/log/application.log
 echo deployVersion is equal to $deployVersion and deployedBuild is $deployedBuild  >> /home/ec2-user/builds/log/application.log
+
+if [ $deployedBuild -eq $deployVersion ]
+then
+	echo "both builds are same, no need to stop the Application" >> /home/ec2-user/builds/log/application.log
+else
+	echo "New build is available, need to stop the Application, executiing stop.sh" >> /home/ec2-user/builds/log/application.log
+fi
