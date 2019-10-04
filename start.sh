69eda1cb91b08f9502639e2bd4e29959e4b184a0
Deepanshu <kukreja@apple.com>
2019-10-04 11:41:13 -0700

    changed

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
+if [ -f  /home/ec2-user/builds/lib/messageUtil-1.0.jar ]
+then
+	deployedBuild=`echo /home/ec2-user/builds/lib/messageUtil-1.0.jar|cut -d '-' -f2|sed 's/.jar//'`
+	deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
+fi
+deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
+
+echo application has been stop
+echo deployVersion is equal to $deployVersion and deployedBuild is $deployedBuild
+
 sh /home/ec2-user/builds/GATEWAY/bin/startup.sh >/dev/null 2>&1 &
 echo "Application has been started"
 echo "\n"
--- stop.sh
+++ stop.sh
@@ -1,1 +1,7 @@
+echo "Checking if the deployed version is same as of build folder"
+
+deployedBuild=`echo /home/ec2-user/builds/lib/messageUtil-1.0.jar|cut -d '-' -f2|sed 's/.jar//'`
+deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
+
 echo application has been stop
+echo deployVersion is equal to $deployVersion and deployedBuild is $deployedBuild
