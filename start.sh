f6e4f3fdb6562ecd7fbb8be2664fb22da83de9e7
Deepanshu <kukreja@apple.com>
2019-10-04 12:08:19 -0700

    changed

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
+mkdir -p /home/ec2-user/builds/log/
 if [ -f  /home/ec2-user/builds/lib/messageUtil-1.0.jar ]
 then
-	deployedBuild=`echo /home/ec2-user/builds/lib/messageUtil-1.0.jar|cut -d '-' -f2|sed 's/.jar//'`
+	deployedBuild=`basename /home/ec2-user/builds/lib/messageUtil*.jar|cut -d '-' -f2|sed 's/.jar//'`
 	deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
 fi
-deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
+deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'` >> /home/ec2-user/builds/log/application.log
 
-echo application has been stop
-echo deployVersion is equal to $deployVersion and deployedBuild is $deployedBuild
+echo application has been stop  >> /home/ec2-user/builds/log/application.log
+echo deployVersion is equal to $deployVersion and deployedBuild is $deployedBuild  >> /home/ec2-user/builds/log/application.log
 
-sh /home/ec2-user/builds/GATEWAY/bin/startup.sh >/dev/null 2>&1 &
-echo "Application has been started"
+sh /home/ec2-user/builds/GATEWAY/bin/startup.sh >/dev/null 2>&1 &  >> /home/ec2-user/builds/log/application.log
+echo "Application has been started"  >> /home/ec2-user/builds/log/application.log
 echo "\n"
--- stop.sh
+++ stop.sh
@@ -1,7 +1,8 @@
 echo "Checking if the deployed version is same as of build folder"
+mkdir -p /home/ec2-user/builds/log/
 
-deployedBuild=`echo /home/ec2-user/builds/lib/messageUtil-1.0.jar|cut -d '-' -f2|sed 's/.jar//'`
+deployedBuild=`basename /home/ec2-user/builds/lib/messageUtil-1.0.jar|cut -d '-' -f2|sed 's/.jar//'`
 deployVersion=`aws ssm get-parameter --name GATEWAY --region us-west-1 --query Parameter.Value|sed 's/"//g'`
 
-echo application has been stop
-echo deployVersion is equal to $deployVersion and deployedBuild is $deployedBuild
+echo application has been stop  >> /home/ec2-user/builds/log/application.log
+echo deployVersion is equal to $deployVersion and deployedBuild is $deployedBuild  >> /home/ec2-user/builds/log/application.log
